ce06635804bf1b3f537f75944232c91da3394341
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-05-08 14:31:18 -0300

    Car entity

---
+++ src/main/java/com/lorram/carrent/entities/Car.java
@@ -0,0 +1,73 @@
+package com.lorram.carrent.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_car")
+public class Car implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer manufactureYear;
+	private String model;
+	
+	public Car() {
+	}
+
+	public Car(Long id, Integer manufactureYear, String model) {
+		this.id = id;
+		this.manufactureYear = manufactureYear;
+		this.model = model;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getYear() {
+		return manufactureYear;
+	}
+
+	public void setYear(Integer year) {
+		this.manufactureYear = year;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		return Objects.equals(id, other.id);
+	}
+}
